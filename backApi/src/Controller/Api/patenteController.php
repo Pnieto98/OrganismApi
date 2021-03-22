ce9dff3424a3ed9b6478e6ade71883bfbae080f3
Pedro Nieto <pedron1998@outlook.com>
2021-03-22 17:19:32 -0300

    Testiando la generación de las solicitud de pago por Api

--- backApi/src/Controller/Api/patenteController.php
+++ backApi/src/Controller/Api/patenteController.php
@@ -3,19 +3,37 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 
 class patenteController extends AbstractController
 {
     /**
      * @Route("/api/patente", name="api_patente")
      */
-    public function index(): Response
+    public function index(HttpClientInterface $client)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/patenteController.php',
+
+        $response = $client->request('POST', 'https://sandbox.epagos.com.ar/post.php', [
+            'body' => [
+                'id_usuario' => '991',
+                'id_organismo' => '83',
+                'password' => '71e017eb858d1c03dae87ba85db6dee6',
+                'hash' => 'b75a6abbc3da3f23e453cace10f35d81'
+            ]
         ]);
+        $solicitudPago = $client->request('POST', 'https://postsandbox.epagos.com.ar', [
+            'body'=>[
+                'detalle_operación' => 'asdasd'
+            ]
+        ]);
+
+        return new JsonResponse(
+            $solicitudPago->toArray()
+        );
+
     }
 }
