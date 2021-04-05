9e637486287a15baf8f963409220d82afae1fdb2
Pedro Nieto <pedron1998@outlook.com>
2021-04-05 16:42:49 -0300

    Mostrando el portal de pagos en una iframe

--- UI/src/app/templates/resultados-deuda/resultados-deuda.component.ts
+++ UI/src/app/templates/resultados-deuda/resultados-deuda.component.ts
@@ -1,27 +1,65 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewChild} from '@angular/core';
-import { config } from 'rxjs';
-import { BuscarDeudaComponent } from 'src/app/Form/buscar-deuda/buscar-deuda.component';
-import { DeudaSeleccionadaService } from 'src/app/Service/deuda-seleccionada.service';
 
+import { Component, Input, OnInit } from '@angular/core';
+import { EpagosApiService } from 'src/app/Service/api/epagos-api.service';
+import { DeudaSeleccionadaService } from 'src/app/Service/deuda-seleccionada.service';
 @Component({
   selector: 'app-resultados-deuda',
   templateUrl: './resultados-deuda.component.html',
-  styleUrls: ['./resultados-deuda.component.css']
+  styleUrls: ['./resultados-deuda.component.css'],
 })
-export class ResultadosDeudaComponent  implements OnInit {
+export class ResultadosDeudaComponent implements OnInit {
   @Input() resultadoDeuda: any;
   isChecked: boolean;
-  constructor(private deudaSelec: DeudaSeleccionadaService) { 
-  }
-  ngOnInit() {
-  }
-  eventCheck(obj, $event){
+  constructor(
+    private deudaSelec: DeudaSeleccionadaService,
+    private epagosService: EpagosApiService
+  ) {}
+  ngOnInit() {}
+  eventCheck(obj, $event) {
     this.deudaSelec.arraySeleccionado;
     this.isChecked = $event.target.checked;
-    if(this.isChecked){
+    if (this.isChecked) {
       this.deudaSelec.agregar(obj);
-    }else{
+    } else {
       this.deudaSelec.borrar(obj);
     }
+  }
+  onClick() {
+   let datosPago = {
+     version : "2.0",
+     operacion: "op_pago",
+     id_organismo: "37",
+     token: "c0d682c02f9072d1f75e408512c3b687",
+     convenio: "",
+     numero_operacion: "123123",
+     id_moneda_operacion: "1",
+     monto_operacion: 2331,
+     detalle_operacion: "",
+     detalle_operacion_visible: "0",
+     ok_url: "https://postsandbox.epagos.com.ar/tests/ok.php" ,
+     error_url: "https://postsandbox.epagos.com.ar/tests/error.php",
+    }
+    let form = document.createElement("form")
+    form.method = "POST";
+    form.action = "https://postsandbox.epagos.com.ar";
+    
+    for(const nombre in datosPago){
+      let input = document.createElement("input");
+      input.type = "hidden";
+      input.name = nombre;
+      input.value = datosPago[nombre];
+      form.appendChild(input);
+    }
+    let contEpago = document.querySelector('.container-epagos');
+    form.target = "epagos";
+    let iframe = document.createElement('iframe');
+    iframe.name = "epagos";
+    iframe.frameBorder="0";
+    iframe.src = "";
+    iframe.style.width = "100%";
+    iframe.style.height = "500px";
+    contEpago.appendChild(iframe);
+    document.body.appendChild(form);
+    form.submit();  
   }
 }
---
+++ backApi/src/Servicios/epagosApi.php
@@ -0,0 +1,131 @@
+<?php
+namespace App\Servicios\Epagos;
+
+use Exception;
+
+define('EPAGOS_ENTORNO_SANDBOX',    0);
+define('EPAGOS_ENTORNO_PRODUCCION', 1);
+
+class epagosApi {
+    const DEBUG_ACTIVADO   = false;
+
+    private $_id_organismo = null;
+    private $_id_usuario   = null;
+  
+    private $_entorno      = EPAGOS_ENTORNO_SANDBOX;
+    private $_cliente      = null;
+    private $_debug        = [];
+  
+    private $_token        = '';
+
+    public function __construct($id_organismo, $id_usuario) {
+        if ($id_organismo === ''){
+          throw new Exception('Debe indicar el ID de organismo recibido para la implementación');
+        }
+        if (!$id_usuario){
+          throw new Exception('Debe indicar el ID de usuario recibido para la implementación');
+        }
+        $this->_id_organismo = $id_organismo;
+        $this->_id_usuario   = $id_usuario;
+      }
+      public function set_entorno($entorno){
+        if (!in_array($entorno, [1, 0])){
+          throw new Exception('Indique un entorno válido');
+        }
+        $this->_entorno = $entorno;
+      }
+      public function obtener_token($password, $hash){
+        if (!$password){
+          throw new Exception('Debe indicar el password recibido para la implementación');
+        }
+        if (!$hash){
+          throw new Exception('Debe indicar el hash recibido para la implementación');
+        }
+    
+        $credenciales = array(
+          'id_usuario'   => $this->_id_usuario,
+          'id_organismo' => $this->_id_organismo,
+          'password'     => $password,
+          'hash'         => $hash
+        );
+    
+        $this->_cliente = new \SoapClient($this->get_url(), array(
+          'soap_version'  => SOAP_1_1,
+          'trace'         => true,
+          'exceptions'    => false,
+          'cache_wsdl'    => WSDL_CACHE_NONE
+        ));
+    
+        if (is_soap_fault($this->_cliente)) {
+          if (self::DEBUG_ACTIVADO){
+            $this->_debug[] = 'obtener_token :: '.$this->_cliente->__getLastResponse();
+          }
+          throw new Exception($this->_cliente->faultcode. ' - ' .$this->_cliente->faultstring);
+        }
+    
+        $resultado = $this->_cliente->obtener_token($this->get_version(), $credenciales);
+    
+        if (self::DEBUG_ACTIVADO) {
+          $this->_debug[] = 'obtener_token :: ' . $this->_cliente->__getLastResponse();
+        }
+    
+        if (is_soap_fault($resultado)) {
+          throw new Exception($this->_cliente->faultcode. ' - ' .$this->_cliente->faultstring);
+        }
+    
+        $this->_token = $resultado['token'];
+        return $resultado;
+      }
+
+        private function get_url(){
+    if ($this->_entorno == EPAGOS_ENTORNO_PRODUCCION)
+      return 'https://api.epagos.com.ar/post.php';
+    else
+      return 'https://sandbox.epagos.com.ar/post.php';
+  }
+  public function obtener_token_post($password, $hash){
+    if (!$password){
+      throw new Exception('Debe indicar el password recibido para la implementación');
+    }
+    if (!$hash){
+      throw new Exception('Debe indicar el hash recibido para la implementación');
+    }
+
+    $fields = [
+      'id_usuario'   => $this->_id_usuario,
+      'id_organismo' => $this->_id_organismo,
+      'password'     => $password,
+      'hash'         => $hash
+    ];
+    $post_field_string = http_build_query($fields, '', '&');
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->get_url_token());
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_field_string);
+    curl_setopt($ch, CURLOPT_POST, true);
+    $response = curl_exec($ch);
+
+    // control de error HTTP
+    if ($response === FALSE) {
+      throw new Exception(curl_error($ch));
+    }
+
+    curl_close($ch);
+    $resultado = json_decode($response);
+    if ($resultado->token)
+      $this->_token = $resultado->token;
+
+    return $resultado;
+  }
+  public function get_version(){
+    return '1.0';
+  }
+  private function get_url_token(){
+    if ($this->_entorno == EPAGOS_ENTORNO_PRODUCCION)
+      return 'https://api.epagos.com.ar/post.php';
+    else
+      return 'https://sandbox.epagos.com.ar/post.php';
+  }
+}
