12ce01cf3fb38f16ddd53662c44be93c201e5d87
Pedro Nieto <pedron1998@outlook.com>
2021-04-12 16:14:46 -0300

    :sparkles: NEW: Mostrar los detalles de operacion en el e-CheckOut

--- API/src/Controller/Api/PagoDeudaController.php
+++ API/src/Controller/Api/PagoDeudaController.php
@@ -11,40 +11,58 @@ use App\Servicios\Epagos\epagosApi;
 class PagoDeudaController extends AbstractController
 {
   private $epagos;
+
   public function __construct()
   {
     $this->epagos = new epagosApi("0", "219");
   }
+
   /**
    * @Route("/api/pago/deuda", name="api_pago_deuda", methods={"POST"})
    */
   public function obtenerSolicitudPago(Request $request)
   {
-    $this->epagos->set_entorno(0);
-    $token = $this->epagos->obtener_token_post("badad406f266a5d93944390eedd20a74", "ac2e7d091343ddd833019a5f82994574");
-    $token = $token->token;
+    $token = $this->obtenerToken();
     $repositorio = $this->getDoctrine()->getRepository(Comercio::class);
+
+    /*Inicializar variables */
     $totalSaldo = 0;
     $numeroOperacion = "";
+    $detalleOperacion = [];
+    $idItem = 0;
+
+    /*Recorriendo los datos obtenidos en POST */
     foreach(json_decode($request->getContent()) as $valor){
       $consultaDeuda = $repositorio->findBy(
         ["id" => $valor->id]);
+
+        /*Chequear si existe el número de cuenta enviado en la BD */
          if($consultaDeuda != null){
           foreach($consultaDeuda as $deuda){  
             $totalSaldo += $deuda->getSaldo();
             $numeroOperacion .= "|".$deuda->getId()."|";
+            $detalleOperacion[] = 
+            [
+              'id_item' => $idItem++,
+              'desc_item'=>$deuda->getPeriodo(),
+              'monto_item'=>$deuda->getSaldo(),
+              'cantidad_item'=>'1'
+            ];
           }
       }
-    
-      
-     
     }
-    
+    /** Datos para pasarle al form en la UI **/
     $datosContribuyente = [
       'saldo' => $totalSaldo,
       'token' => $token,
-      'numero_operacion' => $numeroOperacion
+      'numero_operacion' => $numeroOperacion,
+      'detalle_operacion' =>urlencode(json_encode($detalleOperacion))
     ];
     return new JsonResponse($datosContribuyente);
+  }
+  private function obtenerToken(){
+    $this->epagos->set_entorno(0);
+    $token = $this->epagos->obtener_token_post("badad406f266a5d93944390eedd20a74", "ac2e7d091343ddd833019a5f82994574");
+    return $token->token;
   }
 }
